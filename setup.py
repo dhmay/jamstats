b5715d8b0bcd8549c71e6e970340c5a5f224e9da
dhmay <damon@gurple.com>
2022-12-04 23:12:20 -0800

    added setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+from setuptools import find_packages
+
+setup(name='jamstats',
+      version='0.1',
+      description='Data processing, stats and plots on roller derby scoreboard JSON files',
+      author='Damon May',
+      packages=find_packages())
+     
