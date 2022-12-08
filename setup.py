28f5e501c2ad5addc7f101f707b72b7a93a0556e
dhmay <damon@gurple.com>
2022-12-07 20:50:47 -0800

    fixed setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,16 @@
-#!/usr/bin/env python
+from setuptools import setup, find_packages
 
-from distutils.core import setup
-from setuptools import find_packages
+setup(
+    name='jamstats',
+    version='0.1.0',    
+    description='Data processing, stats and plots on roller derby scoreboard JSON files',
+    author='Damon May',
+    packages=find_packages('src', exclude=['test']),
+    install_requires=['pandas>=1.3.4', 'seaborn>=0.11.2'],
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Derby afficionados',
+        'Programming Language :: Python :: 3',
+    ],
+)
 
-setup(name='jamstats',
-      version='0.1',
-      description='Data processing, stats and plots on roller derby scoreboard JSON files',
-      author='Damon May',
-      packages=find_packages())
-     
