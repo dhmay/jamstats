fb903c4889f1d095b05f099ba2b08f7ad088d9ae
dhmay <damon@gurple.com>
2022-12-28 15:07:45 -0800

    v1.3.4

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import setup, find_packages
 
 setup(
     name='jamstats',
-    version='0.3.3',
+    version='0.3.4',
     description='Data processing, stats and plots on roller derby scoreboard JSON files',
     author='Damon May',
     package_dir={"":"src"},
     include_package_data=True,
     packages=find_packages('src', exclude=['test']),
     scripts=['bin/jamstats'],
-    install_requires=['pandas>=1.3.4', 'seaborn>=0.11.2'],
+    install_requires=['pandas>=1.3.4', 'seaborn>=0.11.2', 'flask>=2.2.2'],
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Programming Language :: Python :: 3',
