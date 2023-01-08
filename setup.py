036404089e8846d36751cff17f5c2e2bf16e6203
dhmay <damon@gurple.com>
2023-01-08 12:49:13 -0800

    v0.4.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='jamstats',
-    version='0.3.4',
+    version='0.4.0',
     description='Data processing, stats and plots on roller derby scoreboard JSON files',
     author='Damon May',
     package_dir={"":"src"},
