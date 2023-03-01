e464eee8d74772de7e6540ba1b207318843c8af8
dhmay <damon@gurple.com>
2023-02-28 17:28:39 -0800

    deps again

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     packages=find_packages('src', exclude=['test']),
     scripts=['bin/jamstats'],
     install_requires=['pandas>=1.3.4', 'seaborn>=0.11.2', 'flask>=2.2.2', 'attrdict>=2.0.1', 'gooey>=1.0.8.1', 'websocket-client>=1.2.1',
-		      'Flask-SocketIO>=5.3.2', 'python-engineio>=4.3.4'],
+		      'Flask-SocketIO>=5.3.2', 'python-engineio>=4.3.4', 'threading', 'time', 'queue'],
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Programming Language :: Python :: 3',
