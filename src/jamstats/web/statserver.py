1307858af472dcf4fdf562ddaf9e67beade1ff0a
dhmay <damon@gurple.com>
2024-01-30 22:44:44 -0800

    added async_mode='gevent' to SocketIO initialization, which seemed to restore browser refresh

--- src/jamstats/web/statserver.py
+++ src/jamstats/web/statserver.py
@@ -191,9 +191,7 @@ def start(port: int, scoreboard_client: ScoreboardClient = None,
     # for communicating with clients
     
     logger.debug("Starting SocketIO Flask app...")
-    # setting async_mode="gevent" here doesn't hurt, but it doesn't seem necessary.
-    # Might be something to try if we get async problems again.
-    app.socketio = SocketIO(app) #, async_mode="gevent")
+    app.socketio = SocketIO(app, async_mode="gevent") # , logger=True, engineio_logger=True)
 
     # add listener to update webclient when game state changes
     if scoreboard_client is not None:
