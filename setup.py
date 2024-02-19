81f119f891827eb9f75148b4ab200b94eba65e5e
dhmay <damon@gurple.com>
2024-02-19 11:37:08 -0800

    changes for 1.4.4

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='jamstats',
-    version='1.4.3',
+    version='1.4.4',
     description='Data processing, stats and plots on roller derby scoreboard JSON files',
     author='Damon May',
     package_dir={"":"src"},
@@ -10,7 +10,7 @@ setup(
     packages=find_packages('src', exclude=['test']),
     scripts=['bin/jamstats', 'bin/jamstats-nogui'],
     install_requires=['pandas>=1.3.4', 'seaborn>=0.11.2', 'flask>=2.2.2',
-                      'wxpython==4.2.0',
+                      'wxpython>=4.2.1', 'eventlet>=0.33.3',
 		      'gooey>=1.0.8.1', 'websocket-client>=1.2.1',
 		      'Flask-SocketIO>=5.3.2', 'python-engineio>=4.3.4', 'gevent>=22.10.2', 'gevent-websocket>=0.10.1',
 		      'dnspython==2.2.1', 'urllib3==1.26.7'],
