212f69d83a6d099ebb6e60eee101b2b1e5147349
dhmay <damon@gurple.com>
2024-02-19 11:42:08 -0800

    change python version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='jamstats',
-    version='1.4.4',
+    version='1.4.5',
     description='Data processing, stats and plots on roller derby scoreboard JSON files',
     author='Damon May',
     package_dir={"":"src"},
